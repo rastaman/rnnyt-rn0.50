04c76d8193322973b8e9d9d87d8352edee02e467
Ludovic Maître <ludovic.maitre@effervens.com>
2017-12-01 18:57:51 +0100

    Chapter 4

---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import HomeScreen from './src/components/HomeScreen';
+
+AppRegistry.registerComponent('RNNYT', () => HomeScreen);
---
+++ src/components/HomeScreen.ios.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {
+  TabBarIOS,
+  Text,
+  Alert,
+  Vibration,
+  StatusBar
+} from 'react-native';
+
+import NewsFeed from './NewsFeed';
+import Search from './Search';
+import * as globalStyles from '../styles/global';
+
+// Set the status bar for iOS to light
+StatusBar.setBarStyle('light-content');
+
+export default class HomeScreen extends Component {
+
+   constructor(props) {
+     super(props);
+     this.state = {
+       tab: 'newsFeed'
+     };
+   }
+
+   showBookmarkAlert() {
+     Vibration.vibrate();
+     Alert.alert(
+       'Coming Soon!',
+       'We\'re hard at work on this feature, check back in the near future.',
+       [
+         { text: 'OK', onPress: () => console.log('User pressed OK') }
+       ]
+     );
+   }
+
+   render() {
+       return (
+         <TabBarIOS
+         barTintColor={globalStyles.BAR_COLOR}
+         tintColor={globalStyles.LINK_COLOR}
+         translucent={false}>
+         <TabBarIOS.Item selected={this.state.tab === 'newsFeed'}
+          systemIcon={'featured'}
+          onPress={() => this.setState({ tab: 'newsFeed' })}
+          badge={4}>
+           <NewsFeed />
+         </TabBarIOS.Item>
+         <TabBarIOS.Item selected={this.state.tab === 'search'}
+            systemIcon={'search'}
+            onPress={() => this.setState({ tab: 'search' })}>
+           <Search />
+         </TabBarIOS.Item>
+         <TabBarIOS.Item selected={this.state.tab === 'bookmarks'}
+            systemIcon={'bookmarks'}
+            onPress={() => this.showBookmarkAlert()}>
+           <Text>Bookmarks</Text>
+         </TabBarIOS.Item>
+         </TabBarIOS>
+       );
+   }
+}
